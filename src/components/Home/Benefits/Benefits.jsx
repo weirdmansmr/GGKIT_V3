c67785fae7fa9d35cefe97683c95739a30f9cb34
WeirdmanSMR <maga.saykhanov95@gmail.com>
2023-12-25 02:04:53 +0300

    almost done with benefits

--- src/components/Home/Benefits/Benefits.jsx
+++ src/components/Home/Benefits/Benefits.jsx
@@ -3,11 +3,23 @@ import { VscGraphLine } from "react-icons/vsc";
 
 const Benefits = () => {
 	return (
-		<section className=" dark:bg-[#673AB7] px-2 py-4 flex flex-col justify-center gap-4 border-t border-yellow-500">
+		<section className=" dark:bg-[#673AB7] px-2 py-4 flex flex-col gap-4 justify-center border-t border-yellow-500">
 			<h2 className="text-xl text-center text-[#512DA8] dark:text-yellow-500 font-semibold">
 				Преимущества обучения в нашей системе
 			</h2>
-			<div className="flex items-center justify-center">
+			<div className="flex items-center justify-center flex-col gap-4">
+				<div className=" w-80 h-80 flex justify-center flex-col items-center border-gradient dark:border-gradient-dark">
+					<VscGraphLine className="fill-[#512DA8] dark:fill-yellow-500 w-14 h-14" />
+					<p className="text-center text-[#512DA8] dark:text-yellow-500">
+						Быстрое развитие и улучшение навыков в необходимой сфере
+					</p>
+				</div>
+				<div className=" w-80 h-80 flex justify-center flex-col items-center border-gradient dark:border-gradient-dark">
+					<VscGraphLine className="fill-[#512DA8] dark:fill-yellow-500 w-14 h-14" />
+					<p className="text-center text-[#512DA8] dark:text-yellow-500">
+						Быстрое развитие и улучшение навыков в необходимой сфере
+					</p>
+				</div>
 				<div className=" w-80 h-80 flex justify-center flex-col items-center border-gradient dark:border-gradient-dark">
 					<VscGraphLine className="fill-[#512DA8] dark:fill-yellow-500 w-14 h-14" />
 					<p className="text-center text-[#512DA8] dark:text-yellow-500">
